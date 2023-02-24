b6a0c60ab177df9f415dc1637833e3f1cd906c4a
aryan <aryanp862002@gmail.com>
2023-02-24 11:55:48 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import KBoard from "./KBoard";
+
+function App() {
+  return (
+    <div className="App">
+      <KBoard></KBoard>
+    </div>
+  );
+}
+
+export default App;
